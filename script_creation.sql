06a5b5f93bc09d96799ae68975a4721834d334b6
alexandrebulatovic <alexandre.bulatovic@gmail.com>
2017-01-17 23:43:11 +0100

    ajout script sql

---
+++ script_creation.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Produits
+(
+id_produit INT NOT NULL,
+nom VARCHAR(255) NOT NULL UNIQUE,
+quantite INT NOT NULL,
+prix NUMBER(*,2) NOT NULL,
+CONSTRAINT pk_produit PRIMARY KEY (id_produit)
+)
+
+CREATE SEQUENCE id_produit_sequence;
+
+CREATE OR REPLACE TRIGGER auto_increment_id_produit
+  BEFORE INSERT ON Produits
+  FOR EACH ROW
+BEGIN
+  SELECT id_produit_sequence.NEXTVAL
+  INTO :new.id_produit
+  FROM Dual;
+END;
